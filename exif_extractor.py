681ee4ab2979387ce6960d1b2c8f750dea2112d5
garygtdu <garygtdu@gmail.com>
2026-09-22 22:35:56 -0400

    updated file name and iso finder to general field finder

---
+++ exif_extractor.py
@@ -0,0 +1,60 @@
+# Extracting exif from photo files
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS, GPS
+from pathlib import Path
+import os
+
+
+photodata = []
+
+for photofile in os.listdir("Photos"):
+    with Image.open(Path("Photos") / photofile) as file:
+        exif_data = file._getexif()
+        if exif_data is None:
+            continue
+        else: 
+            photodata.append(exif_data)
+
+
+def exif_extractor(data: list):
+
+    datalibrary = []
+
+    for entry in data:
+
+        individual_photo = {}
+
+        for tag, value in entry.items():
+            individual_photo[TAGS[tag]] = value
+        datalibrary.append(individual_photo)
+
+    return datalibrary
+
+readable_data = exif_extractor(photodata)
+
+
+
+# Camera settings finder!!!
+
+def field_finder(readable_data: list, field_list: list):
+
+    setting = []
+
+    for entry in readable_data:
+
+        photo_dict = {}
+
+        for field_name in field_list: 
+
+            if field_name in entry:
+                photo_dict[field_name] = entry[field_name]
+
+        setting.append(photo_dict)
+    
+    return setting
+
+
+# Test!!
+# fields = ['ISOSpeedRatings', 'FNumber', 'ShutterSpeedValue', 'FocalLength']
+#camera_settings = field_finder(readable_data, fields)
+# print(camera_settings)
